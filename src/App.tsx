94bf56f6e662d028b9ff7709f5b3014e08663eff
Aries <lehuy25032004@gmail.com>
2025-07-01 15:57:05 +0700

    feat: add admin page

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ import BlogDetail from './pages/BlogList/BlogDetail'
 import UserLayout from './layouts/UserLayout'
 import Podcast from './pages/Podcast/Podcast'
 import Profile from './pages/Profile/Profile'
+import AdminLayout from './layouts/AdminLayout'
+import { AdminDashboard } from './pages/Admin/AdminPage'
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' />
+      <Route path='/' element={<UserLayout><BlogList /></UserLayout>}  />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/chat' element={<UserLayout><Chat /></UserLayout>} />
@@ -21,6 +23,7 @@ function App() {
       <Route path='/blog/:id' element={<UserLayout><BlogDetail /></UserLayout>} />
       <Route path='/podcast' element={<UserLayout><Podcast /></UserLayout>} />
       <Route path='/profile' element={<UserLayout><Profile /></UserLayout>} />
+      <Route path='/admin' element={<AdminLayout><AdminDashboard /></AdminLayout>} />
     </Routes>
   )
 }
